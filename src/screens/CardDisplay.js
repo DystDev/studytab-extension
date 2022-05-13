1574000fc8d2a50373c961dfc4a90c7403b4a0ea
DystDev <58528580+DystDev@users.noreply.github.com>
2022-05-13 17:31:56 +0100

    Fix linting

--- src/screens/CardDisplay.js
+++ src/screens/CardDisplay.js
@@ -5,6 +5,10 @@ import MainCard from '../components/MainCard.js';
 const CardDisplay = (props) => {
   const [currentPrompt, setCurrentPrompt] = useState(null);
 
+  chrome.storage.sync.get(['key'], function (result) {
+    console.log('Value currently is ' + result.key);
+  });
+
   return (
     <div className="cardDisplayScreen">
       {currentPrompt != null ? (
